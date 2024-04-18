7991bb9f858b35811ac30d2a34cb162572572ed3
premSinghDhoni <premsinghdhoni35@gmail.com>
2024-04-18 17:49:22 +0530

    made loader sucessfully

---
+++ script.js
@@ -0,0 +1,48 @@
+//loader
+let tl=gsap.timeline();
+
+tl.from('.line h1',{
+  y:150,
+  stagger:0.2,
+  duration:0.8,
+  delay:0.5,
+})
+
+tl.from('#line1-part1',{
+  opacity:0,
+  onStart:function(){
+    let loadtimer=document.querySelector('#line1-part1 h5');
+let count=0;
+let time=setInterval(()=>{
+  count++;
+  if(count <= 100){
+  loadtimer.textContent=count;
+ }
+ else{
+   clearInterval(time);
+ }
+},33)
+  }
+})
+tl.to('.line h2',{
+  animationName:'anime',
+  opacity:1,
+ 
+})
+tl.to('#loader',{
+  opacity:0,
+  duration:0.4,
+  delay:3.5,
+  ease:Power4,
+})
+
+//page1
+tl.from('#page1',{
+  delay:0.3,
+  opacity:0,
+  y:1500,
+  duration:0.5,
+})
+tl.to('#loader',{
+  display:'none',
+})
